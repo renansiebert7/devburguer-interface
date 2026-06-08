42989362c5b5ab6d30b6affa2a8b1615683d5f63
renansiebert7 <renansiebert01@gmail.com>
2026-06-08 20:55:18 -0300

    ajuste scroll footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     background-color: #5c2669;
     height: 50px;
-    width: 100vw;
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
