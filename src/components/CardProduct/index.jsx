812bc2c54354939405e9da31301ed42d28a70fb1
renansiebert7 <renansiebert01@gmail.com>
2026-06-08 20:07:58 -0300

    commit responsividade

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types"
 import { Container, CardImage, CardButton } from "./styles.js"
 import { useCart } from "../../hooks/CartContext"
-import { CardButton } from "../CardButton"
 import { formatPrice } from "../../utils/formatPrice.js"
 
 export function CardProduct({product}){
